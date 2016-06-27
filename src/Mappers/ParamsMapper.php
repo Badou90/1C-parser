9ed9e0f66c7af1acc743f9d2b0de776ca1ad119e
Vyacheslav Goryachikh <badou@yandex.ru>
2016-06-27 15:28:10 +0900

    Added Params mapper

--- src/Mappers/ParamsMapper.php
+++ src/Mappers/ParamsMapper.php
@@ -3,11 +3,39 @@
 namespace Badou\Parser\Mappers;
 
 use Badou\Parser\Contracts\ParamsMapperInterface;
+use App\Models\CatalogItem;
 
 class ParamsMapper implements ParamsMapperInterface
 {
+    protected $prices;
+
+    const DEALER_PRICE = "Дилер";
+    const PRICE = "Розница";
+
     public function parse($offer)
     {
-        echo self::class." - base mapper\n";
+        foreach ($offer->ПакетПредложений->ТипыЦен->ТипЦены as $priceType) {
+            $this->prices[(String)$priceType->Ид] = (String)$priceType->Наименование;
+        }
+
+        foreach ($offer->ПакетПредложений->Предложения->Предложение as $offerItem) {
+            $catalogItem = CatalogItem::where('code', $offerItem->Ид)->first();
+            $offerItemPrices = $this->getPrices($offerItem);
+
+            if($catalogItem){
+                $catalogItem->update([
+                    'price' => (isset($offerItemPrices[self::PRICE]))?$offerItemPrices[self::PRICE]:0,
+                    'price_dealer' => (isset($offerItemPrices[self::DEALER_PRICE]))?$offerItemPrices[self::DEALER_PRICE]:0,
+                ]);
+            }
+        }
+    }
+
+    public function getPrices($item)
+    {
+        foreach($item->Цены->Цена as $priceType) {
+            $prices[$this->prices[(String)$priceType->ИдТипаЦены]] = (int)$priceType->ЦенаЗаЕдиницу;
+        }
+        return $prices;
     }
 }
